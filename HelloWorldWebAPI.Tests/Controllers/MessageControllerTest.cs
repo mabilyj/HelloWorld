bdf0aee80b756aa44da6ded6fbd1ca6cabbd2633
Michael Bilyj <mbilyjwork@att.net>
2017-03-02 12:59:42 -0500

    Add files via upload

---
+++ HelloWorldWebAPI.Tests/Controllers/MessageControllerTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HelloWorldWebAPI;
+using HelloWorldWebAPI.Controllers;
+
+
+
+namespace HelloWorldWebAPI.Tests.Controllers
+{
+    [TestClass]
+    public class MessageControllerTest
+    {
+        [TestMethod]
+        public void Get()
+        {
+            // Arrange
+            MessageController controller = new MessageController();
+
+            // Act
+           string result = controller.GetMessage();
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Hello World", result);
+            
+        }
+    }
+}
