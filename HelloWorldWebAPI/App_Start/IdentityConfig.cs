3e0a8194b6558cd96d51438cae673ea887d2d80c
Michael Bilyj <mbilyjwork@att.net>
2017-03-02 13:03:50 -0500

    Add files via upload

---
+++ HelloWorldWebAPI/App_Start/IdentityConfig.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+
+
+namespace HelloWorldWebApi
+{
+    // Configure the application user manager used in this application. UserManager is defined in ASP.NET Identity and is used by the application.
+
+}
---
+++ HelloWorldWebAPI/Controllers/MessageController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using HelloWorld.Data;
+
+namespace HelloWorldWebAPI.Controllers
+{
+    
+    public class MessageController : ApiController
+    {
+        // GET api/Message
+        public string GetMessage()
+        {
+            return HelloWorldData.Message;
+        }
+
+       
+    }
+}
